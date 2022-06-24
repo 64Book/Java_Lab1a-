9ed7f98294b7f069328baf615351915012baba80
64Book <booknarawich2001@gmail.com>
2022-06-24 16:18:58 +0700

    first commit

---
+++ src/P9/I.java
@@ -0,0 +1,14 @@
+package P9;
+
+public class I {
+    public static void main(String[] args) {
+        int dec = 152; // no prefix --> decimal literal
+        int bin = 0b10011000; // '0b' prefix --> binary literal
+        int oct = 0230; // '0' prefix --> octal literal
+        int hex = 0x98; // '0x' prefix --> hexadecimal literal
+        System.out.println("Decimal = " + dec);
+        System.out.println("Binary = " + bin);
+        System.out.println("Octal = " + oct);
+        System.out.println("hexadecimal = " + hex);
+    }
+}
